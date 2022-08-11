3c395e889a436a446736e6f37f3c9f82bacd5f30
sean.sullivan <sean.sullivan@beatport.com>
2022-08-11 09:04:31 -0600

    Added stream model spec.

--- spec/models/stream_spec.rb
+++ spec/models/stream_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Stream, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Create data object for testing
+  test_stream = FactoryBot.create(:stream)
+
+  # Test various conditions
+  it "is valid with valid attributes" do
+    expect(test_stream).to be_valid
+  end
+  it "is not valid without a name" do
+    test_stream.name = nil
+    expect(test_stream).to_not be_valid
+  end
 end
